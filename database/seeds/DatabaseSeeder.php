c0c44c9cb63899d96539963d58f9dd212150ca1f
Susan Buck <susan@susanbuck.net>
2015-11-04 17:54:55 -0500

    Seeder for books table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(BooksTableSeeder::class);
+        //$this->call(AuthorTableSeeder::class);
 
         Model::reguard();
     }
