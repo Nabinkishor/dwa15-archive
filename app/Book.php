193a1361e1fe98dd254857d02c7b235a77d60f4e
Susan Buck <susan@susanbuck.net>
2015-11-11 00:30:23 -0500

    Removed notes.

--- app/Book.php
+++ app/Book.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
-1. naming convention
-table: books
-model: Book
-
-table: authors
-model: Author
-
-table: geese
-model: Goose
-
-2. tables have to have `id`
-auto incrementing
-primary key
-
-3. created_at / updated_at
-
-*/
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
