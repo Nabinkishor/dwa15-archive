571ee4a20bc544433cda3062daf65e3d746bd16f
Susan Buck <susan@susanbuck.net>
2015-11-18 14:36:12 -0500

    Book/Tag relationships defined in the models.

--- app/Book.php
+++ app/Book.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     public function author() {
-       # Book belongs to Author
-       # Define an inverse one-to-many relationship.
-       return $this->belongsTo('\App\Author');
-   }
+        # Book belongs to Author
+        # Define an inverse one-to-many relationship.
+        return $this->belongsTo('\App\Author');
+    }
+
+    public function tags() {
+        return $this->belongsToMany('\App\Tag')->withTimestamps();;
+    }
 }
