298fdf4d7c48bd2d005c47f3456973f90713ce95
Susan Buck <susan@susanbuck.net>
2015-11-17 23:11:06 -0500

    jQuery should be loaded before bootstrap.js.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -56,8 +56,9 @@
         <a href='http://foobooks.dwa15-practice.biz/' class='fa fa-link' target='_blank'> View Live</a>
     </footer>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
 
     {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
     @yield('body')
